6e77535300b84f6c08161858813296c42bcac7fa
rkej <rahulkejriwal18@gmail.com>
2024-12-26 00:41:15 +0530

    proto quick login change

---
+++ src/renderer/src/useXtsAuth.ts
@@ -0,0 +1,17 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query'
+import { AxiosResponse } from 'axios'
+
+const authenticateXts = async (): Promise<AxiosResponse> => {
+  const response = await window.electron.ipcRenderer.invoke('login')
+  console.log(response, 'new response')
+  return await response
+}
+
+export const useXtsAuth = (): UseQueryResult => {
+  return useQuery({
+    queryKey: ['auth', 'xts'],
+    queryFn: () => authenticateXts(),
+    retry: 0,
+    refetchOnWindowFocus: false
+  })
+}
